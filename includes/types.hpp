767a42ac4dec53797addffac11b56f2f6570dbf1
Chinimala <astrid.allemandou@free.fr>
2020-09-18 04:52:00 +0200

    Improve tests

--- includes/types.hpp
+++ includes/types.hpp
@@ -11,6 +11,17 @@ struct is_integer
 };
 
 template<> struct is_integer<bool> {typedef INT_TYPE type;};
+template<> struct is_integer<char> {typedef INT_TYPE type;};
+template<> struct is_integer<signed char> {typedef INT_TYPE type;};
+template<> struct is_integer<unsigned char> {typedef INT_TYPE type;};
+template<> struct is_integer<wchar_t> {typedef INT_TYPE type;};
+template<> struct is_integer<short> {typedef INT_TYPE type;};
+template<> struct is_integer<unsigned short> {typedef INT_TYPE type;};
 template<> struct is_integer<int> {typedef INT_TYPE type;};
+template<> struct is_integer<unsigned int> {typedef INT_TYPE type;};
+template<> struct is_integer<long> {typedef INT_TYPE type;};
+template<> struct is_integer<unsigned long> {typedef INT_TYPE type;};
+template<> struct is_integer<long long> {typedef INT_TYPE type;};
+template<> struct is_integer<unsigned long long> {typedef INT_TYPE type;};
 
 #endif // TYPES_HPP
