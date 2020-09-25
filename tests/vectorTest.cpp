9be7d27e240bc93583769b33df13f03d97f977ba
Chinimala <astrid.allemandou@free.fr>
2020-09-25 11:53:23 +0200

    vector : as add tests

--- tests/vectorTest.cpp
+++ tests/vectorTest.cpp
@@ -23,7 +23,7 @@ print_vector(T & vector, std::string const & name)
 		std::cout << *begin << " ";
 		++begin;
 	}
-	std::cout << "] Size = " << vector.size() << ENDL;
+	std::cout << "] Size : " << vector.size() << ENDL;
 }
 
 template <template <class T, class Alloc = std::allocator<T> > class vec>
@@ -36,21 +36,21 @@ test_vector(void)
 	FILE << CATEGORY << "===> Default constructor" << ENDL;
 	FILE << "vec<int> vectorA;" << ENDL; vec<int> vectorA;
 	print_vector(vectorA, "vectorA");
-	//FILE << "vectorA.max_size() = " << OUTPUT << vectorA.max_size() << ENDL;
-	//FILE << std::boolalpha << "vectorA.empty() = " << OUTPUT << vectorA.max_size() << ENDL;
+	//FILE << "vectorA.max_size() : " << OUTPUT << vectorA.max_size() << ENDL;
+	//FILE << std::boolalpha << "vectorA.empty() : " << OUTPUT << vectorA.max_size() << ENDL;
 	FILE << ENDL;
 
 	FILE << CATEGORY << "===> Fill constructor" << ENDL;
 	FILE << "vec<int> vectorB(3, 2);" << ENDL; vec<int> vectorB(3, 2);
 	print_vector(vectorB, "vectorB");
-	//FILE << std::boolalpha << "vectorA.empty() = " << OUTPUT << vectorA.max_size() << ENDL;
+	//FILE << std::boolalpha << "vectorA.empty() : " << OUTPUT << vectorA.max_size() << ENDL;
 	FILE << ENDL;
 
 	FILE << CATEGORY << "===> Range constructor" << ENDL;
 	FILE << "int intArrayA[] = {1, 2, 3, 4, 5};" << ENDL; int intArrayA[] = {1, 2, 3, 4, 5};
 	FILE << "vec<int> vectorC(intArrayA, intArrayA + 5);" << ENDL; vec<int> vectorC(intArrayA, intArrayA + 5);
 	print_vector(vectorC, "vectorC");
-	//FILE << std::boolalpha << "vectorA.empty() = " << OUTPUT << vectorA.max_size() << ENDL;
+	//FILE << std::boolalpha << "vectorA.empty() : " << OUTPUT << vectorA.max_size() << ENDL;
 	FILE << ENDL;
 
 	FILE << CATEGORY << "===> Copy constructor" << ENDL;
@@ -77,51 +77,79 @@ test_vector(void)
 	print_vector(vectorA, "vectorA");
 	FILE << "vectorA.begin() == vectorA.end() : " << OUTPUT << std::boolalpha << (vectorA.begin() == vectorA.end()) << ENDL;
 	FILE << SUBCATEGORY << "(Pushing some values)" << ENDL;
-	vectorA.push_back(1); vectorA.push_back(2); vectorA.push_back(3);
+	vectorA.push_back(1); vectorA.push_back(2); vectorA.push_back(3); vectorA.push_back(4);
 	print_vector(vectorA, "vectorA");
 	FILE << "vec<int>::iterator it = vectorA.begin();" << ENDL; typename vec<int>::iterator it = vectorA.begin();
 	FILE << "vec<int>::iterator ite = vectorA.end();" << ENDL; typename vec<int>::iterator ite = vectorA.end();
 	//FILE << "(*it == vectorA.front()) = " << OUTPUT << std::boolalpha << (*it == vectorA.front()) << ENDL;
+	FILE << "it == vectorA.end() : " << OUTPUT << std::boolalpha << (it == vectorA.end()) << ENDL;
+	FILE << "it != vectorA.end() : " << OUTPUT << std::boolalpha << (it != vectorA.end()) << ENDL;
 	FILE << "while (it != ite) {...; it++;}" << ENDL;
 	while (it != ite)
 	{
 		FILE << "*it = " << OUTPUT << *it << ENDL;
-		it++;
+		++it;
 	}
+	FILE << "it == vectorA.end() : " << OUTPUT << std::boolalpha << (it == vectorA.end()) << ENDL;
+	FILE << "it != vectorA.end() : " << OUTPUT << std::boolalpha << (it != vectorA.end()) << ENDL;
 	FILE << "it--;" << ENDL; it--;
+	FILE << "*it : " << OUTPUT << *it << ENDL;
+	FILE << "--it;" << ENDL; --it;
+	FILE << "*it : " << OUTPUT << *it << ENDL;
+	FILE << "it++;" << ENDL; it++;
+	FILE << "*it : " << OUTPUT << *it << ENDL;
+	FILE << "--it; --it;" << ENDL; --it; --it;
 	//FILE << "(*it == vectorA.back()) = " << OUTPUT << std::boolalpha << (*it == vectorA.back()) << ENDL;
 	FILE << "*it = 42;" << ENDL; *it = 42;
 	print_vector(vectorA, "vectorA");
+	FILE << "*it : " << OUTPUT << *it << ENDL;
+	FILE << "*(it + 2) : " << OUTPUT << *(it + 2) << ENDL;
+	FILE << "*(it - 1) : " << OUTPUT << *(it - 1) << ENDL;
+	//FILE << "*(2 + it) : " << OUTPUT << *(2 + it) << ENDL;
+	FILE << "ite - it : " << OUTPUT << (ite - it) << ENDL;
+	FILE << "*(it += 2) : " << OUTPUT << *(it += 2) << ENDL;
+	FILE << "*(it -= 2) : " << OUTPUT << *(it -= 2) << ENDL;
+	FILE << "(it += 42) = vectorA.begin();" << ENDL; (it += 42) = vectorA.begin();
+	FILE << "*it : " << OUTPUT << *it << ENDL;
+	FILE << "(it -= 42) = --vectorA.end();" << ENDL; (it -= 42) = --vectorA.end();
+	FILE << "*it : " << OUTPUT << *it << ENDL;
+	FILE << "it[0] : " << OUTPUT << it[0] << ENDL;
+	FILE << "it[-2] : " << OUTPUT << it[-2] << ENDL;
+	FILE << "vec<int>::iterator const const_it = vectorA.begin();" << ENDL;
+	typename vec<int>::iterator const const_it = vectorA.begin();
+	FILE << "const_it[0] : " << OUTPUT << const_it[0] << ENDL;
+	FILE << "const_it[2] : " << OUTPUT << const_it[2] << ENDL;
 	FILE << ENDL;
 
 	FILE << SUBTITLE << "Element access" << ENDL;
 	FILE << CATEGORY << "===> operator[]" << ENDL;
 	FILE << "vec<int> test(2, 1);" << ENDL; vec<int> test(2, 1);
-	FILE << "test[0] : " << test[0] << ENDL;
+	FILE << "test[0] : " << OUTPUT << test[0] << ENDL;
 	FILE << "test[0] = 21;" << ENDL; test[0] = 21;
-	FILE << "test[0] : " << test[0] << ENDL;
+	FILE << "test[0] : " << OUTPUT << test[0] << ENDL;
 	FILE << "test[1] = 7;" << ENDL; test[1] = 7;
-	FILE << "test[1] : " << test[1] << ENDL;
+	FILE << "test[1] : " << OUTPUT << test[1] << ENDL;
 	FILE << "vec<int> const const_test(1, 42);" << ENDL; vec<int> const const_test(1, 42);
-	FILE << "const_test[0] : " << const_test[0] << ENDL;
+	FILE << "const_test[0] : " << OUTPUT << const_test[0] << ENDL;
 	FILE << CATEGORY << "===> at" << ENDL;
-	FILE << "test.at(0) : " << test.at(0) << ENDL;
+	FILE << "test.at(0) : " << OUTPUT << test.at(0) << ENDL;
 	try
 		{FILE << "test.at(42) : " << test.at(42) << ENDL;}
 	catch (std::out_of_range const & e)
-		{FILE << e.what() << ENDL;}
+		{FILE << OUTPUT << e.what() << ENDL;}
 	FILE << "const_test.at(0) : " << const_test.at(0) << ENDL;
 	try
 		{FILE << "const_test.at(-1) : " << const_test.at(static_cast<unsigned long>(-1)) << ENDL;}
 	catch (std::out_of_range const & e)
-		{FILE << e.what() << ENDL;}
+		{FILE << OUTPUT << e.what() << ENDL;}
+
 	FILE << TITLE << "=> ENDING vector tests" << ENDL << ENDL;
 }
 
 int
 main(void)
 {
-    test_vector<ft::vector>();
+    test_vector<std::vector>();
 	/*std::vector<int> test;
 	test.push_back(0);
 	test.push_back(1);
