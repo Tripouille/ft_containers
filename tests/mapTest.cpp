7a0e3d28f6fc38a500caf063c8a9dad5a0778e40
Chinimala <astrid.allemandou@free.fr>
2020-10-24 03:55:20 +0200

    map : add copy constructor test

--- tests/mapTest.cpp
+++ tests/mapTest.cpp
@@ -12,6 +12,7 @@
 #define FILE std::cout << RESET_ALL
 using std::pair;
 using std::make_pair;
+using std::string;
 
 template<class T>
 void
@@ -34,7 +35,7 @@ test_map(void)
 {
 	FILE << TITLE << std::endl << "=> STARTING map tests" << ENDL;
 
-	map<int, std::string> m2;
+	map<int, string> m2;
 	m2[10] = "";
 	m2[7] = "";
 	m2[7] = "";
@@ -59,16 +60,16 @@ test_map(void)
 		std::cout << "reverse(" << end->first << " : " << end->second << ") ";
 	}*/
 
-	typename map<int, std::string>::reverse_iterator rbegin = m2.rbegin();
-	typename map<int, std::string>::reverse_iterator rend = m2.rend();
+	typename map<int, string>::reverse_iterator rbegin = m2.rbegin();
+	typename map<int, string>::reverse_iterator rend = m2.rend();
 	while (rbegin != rend)
 	{
 		std::cout << "reverse(" << rbegin->first << " : " << rbegin->second << ") ";
 		++rbegin;
 	}
 
-	typename map<int, std::string>::const_reverse_iterator crbegin = m2.rbegin();
-	typename map<int, std::string>::const_reverse_iterator crend = m2.rend();
+	typename map<int, string>::const_reverse_iterator crbegin = m2.rbegin();
+	typename map<int, string>::const_reverse_iterator crend = m2.rend();
 	while (crbegin != crend)
 	{
 		std::cout << "reverse(" << crbegin->first << " : " << crbegin->second << ") ";
@@ -78,12 +79,12 @@ test_map(void)
 
 	//begin->first = 1;
 
-	/*map<int, std::string> m2;
+	/*map<int, string> m2;
 	print_map(m2, "m2");
-	typename map<int, std::string>::iterator begin = m2.begin();
+	typename map<int, string>::iterator begin = m2.begin();
 	++begin;*/
 
-	/*typename map<int, std::string>::const_iterator cbegin = m2.begin();
+	/*typename map<int, string>::const_iterator cbegin = m2.begin();
 	FILE << "*cbegin : " << OUTPUT << cbegin->first << ENDL;*/
 	//cbegin->second = "A";
 
@@ -92,35 +93,35 @@ test_map(void)
 
 	FILE << SUBTITLE << "Constructors" << ENDL;
 	FILE << CATEGORY << "===> Default constructor" << ENDL;
-	FILE << "map<int, std::string> mapA;" << ENDL; map<int, std::string> mapA;
+	FILE << "map<int, string> mapA;" << ENDL; map<int, string> mapA;
 	print_map(mapA, "mapA");
 	//FILE << std::boolalpha << "mapA.empty() = " << OUTPUT << mapA.empty() << ENDL;
 	FILE << ENDL;
 
 	FILE << CATEGORY << "===> Range constructor" << ENDL;
-	FILE << "pair<int, std::string> pairArray[] = {make_pair(1, \"A\"), make_pair(2, \"B\"), make_pair(3, \"C\")};" << ENDL;
-	pair<int, std::string> pairArray[] = {make_pair(1, "A"), make_pair(2, "B"), make_pair(3, "C")};
-	FILE << "map<int, std::string> mapB(pairArray, pairArray + 3);" << ENDL;
-	map<int, std::string> mapB(pairArray, pairArray + 3);
+	FILE << "pair<int, string> pairArray[] = {make_pair(1, \"A\"), make_pair(2, \"B\"), make_pair(3, \"C\")};" << ENDL;
+	pair<int, string> pairArray[] = {make_pair(1, "A"), make_pair(2, "B"), make_pair(3, "C")};
+	FILE << "map<int, string> mapB(pairArray, pairArray + 3);" << ENDL;
+	map<int, string> mapB(pairArray, pairArray + 3);
 	print_map(mapB, "mapB");
 	//FILE << std::boolalpha << "mapB.empty() = " << OUTPUT << mapB.empty() << ENDL;
-	FILE << "map<int, std::string> mapC(pairArray, pairArray);" << ENDL;
-	map<int, std::string> mapC(pairArray, pairArray);
+	FILE << "map<int, string> mapC(pairArray, pairArray);" << ENDL;
+	map<int, string> mapC(pairArray, pairArray);
 	print_map(mapC, "mapC");
 	//FILE << std::boolalpha << "mapC.empty() = " << OUTPUT << mapC.empty() << ENDL;
-	FILE << "pair<int, std::string> pairArray2[] = {make_pair(1, \"A\"), make_pair(2, \"B\"), make_pair(3, \"C\")};" << ENDL;
-	pair<int, std::string> pairArray2[] = {make_pair(10, ""), make_pair(7, ""), make_pair(7, ""), make_pair(9, ""), make_pair(8, ""), make_pair(20, ""), make_pair(30, ""), make_pair(21, ""), make_pair(22, ""), make_pair(23, ""), make_pair(33, ""), make_pair(32, ""), make_pair(33, "")};
-	FILE << "map<int, std::string> mapD(pairArray2, pairArray2 + 13);" << ENDL;
-	map<int, std::string> mapD(pairArray2, pairArray2 + 13);
+	FILE << "pair<int, string> pairArray2[] = {make_pair(10, \"\"), make_pair(7, \"\"), make_pair(7, \"\"), make_pair(9, \"\"), make_pair(8, \"\"), make_pair(20, \"\"), make_pair(30, \"\"), make_pair(21, \"\"), make_pair(22, \"\"), make_pair(23, \"\"), make_pair(33, \"\"), make_pair(32, \"\"), make_pair(31, \"\")};" << ENDL;
+	pair<int, string> pairArray2[] = {make_pair(10, ""), make_pair(7, ""), make_pair(7, ""), make_pair(9, ""), make_pair(8, ""), make_pair(20, ""), make_pair(30, ""), make_pair(21, ""), make_pair(22, ""), make_pair(23, ""), make_pair(33, ""), make_pair(32, ""), make_pair(31, "")};
+	FILE << "map<int, string> mapD(pairArray2, pairArray2 + 13);" << ENDL;
+	map<int, string> mapD(pairArray2, pairArray2 + 13);
 	print_map(mapD, "mapD");
 	//FILE << std::boolalpha << "mapD.empty() = " << OUTPUT << mapD.empty() << ENDL;
 	FILE << ENDL;
 
 	FILE << CATEGORY << "===> Copy constructor" << ENDL;
-	/*print_map(mapB, "mapB");
-	FILE << "map<int> mapC(mapB);" << ENCL; vec<int> mapC(mapB);
-	print_map(mapB, "mapB");
-	print_map(mapC, "mapC");*/
+	print_map(mapD, "mapD");
+	FILE << "map<int, string> mapE(mapD);" << ENDL; map<int, string> mapE(mapD);
+	print_map(mapD, "mapD");
+	print_map(mapE, "mapE");
 	FILE << ENDL;
 
 	FILE << TITLE << "=> ENDING map tests" << ENDL << ENDL;
