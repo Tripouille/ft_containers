7f9a139a275bd6910b15e3e81f01041960586e31
jgambard <jmichel.gambard@gmail.com>
2020-11-01 14:58:01 +0100

    stack : add test for relational operators and add test with stack with vector container

--- tests/stackTest.cpp
+++ tests/stackTest.cpp
@@ -21,7 +21,6 @@ test_stack(void)
 	FILE << TITLE << std::endl << "=> STARTING stack tests" << ENDL;
 	FILE << TITLE << std::endl << "=> deque" << ENDL;
 
-	
 	FILE << "stack<int> stackA;" << ENDL; stack<int> stackA;
 	FILE << "stackA.empty() : " << std::boolalpha << OUTPUT << stackA.empty() << ENDL;
 	FILE << "stackA.size() : " << OUTPUT << stackA.size() << ENDL;
@@ -47,19 +46,75 @@ test_stack(void)
 	FILE << "stackB = stackA;" << ENDL; stackB = stackA;
 	FILE << "stackB.top() : " << OUTPUT << stackB.top() << ENDL;
 
+	FILE << ENDL;
 	FILE << "stack<int> const stackConst(stackA);" << ENDL; stack<int> const stackConst(stackA);
 	FILE << "stackConst.top() : " << OUTPUT << stackConst.top() << ENDL;
 	FILE << "stackConst.empty() : " << std::boolalpha << OUTPUT << stackConst.empty() << ENDL;
 	FILE << "stackConst.size() : " << OUTPUT << stackConst.size() << ENDL;
 
+	FILE << ENDL;
 	FILE << "stack<int> stackEmpty;" << ENDL; stack<int> stackEmpty;
 	FILE << "stackConst == stackA : " << std::boolalpha << OUTPUT << (stackConst == stackA) << ENDL;
+	FILE << "stackConst == stackEmpty : " << std::boolalpha << OUTPUT << (stackConst == stackEmpty) << ENDL;
+	FILE << "stackConst != stackA : " << std::boolalpha << OUTPUT << (stackConst != stackA) << ENDL;
+	FILE << "stackConst != stackEmpty : " << std::boolalpha << OUTPUT << (stackConst != stackEmpty) << ENDL;
+	FILE << "stackConst < stackA : " << std::boolalpha << OUTPUT << (stackConst < stackA) << ENDL;
+	FILE << "stackEmpty < stackConst: " << std::boolalpha << OUTPUT << (stackEmpty < stackConst) << ENDL;
+	FILE << "stackConst <= stackA : " << std::boolalpha << OUTPUT << (stackConst <= stackA) << ENDL;
+	FILE << "stackEmpty <= stackConst: " << std::boolalpha << OUTPUT << (stackEmpty <= stackConst) << ENDL;
+	FILE << "stackConst > stackA : " << std::boolalpha << OUTPUT << (stackConst > stackA) << ENDL;
+	FILE << "stackEmpty > stackConst: " << std::boolalpha << OUTPUT << (stackEmpty > stackConst) << ENDL;
+	FILE << "stackConst >= stackA : " << std::boolalpha << OUTPUT << (stackConst >= stackA) << ENDL;
+	FILE << "stackEmpty >= stackConst: " << std::boolalpha << OUTPUT << (stackEmpty >= stackConst) << ENDL;
 
 
+	FILE << TITLE << std::endl << "=> vector" << ENDL;
 
+	FILE << "stack<int, ft::vector<int> > vectorstackA;" << ENDL; stack<int, ft::vector<int> > vectorstackA;
+	FILE << "vectorstackA.empty() : " << std::boolalpha << OUTPUT << vectorstackA.empty() << ENDL;
+	FILE << "vectorstackA.size() : " << OUTPUT << vectorstackA.size() << ENDL;
 
+	FILE << "vectorstackA.push(0);" << ENDL; vectorstackA.push(0);
+	FILE << "vectorstackA.top() : " << OUTPUT << vectorstackA.top() << ENDL;
+	FILE << "vectorstackA.empty() : " << std::boolalpha << OUTPUT << vectorstackA.empty() << ENDL;
+	FILE << "vectorstackA.size() : " << OUTPUT << vectorstackA.size() << ENDL;
 
+	FILE << "vectorstackA.push(1);" << ENDL; vectorstackA.push(1);
+	FILE << "vectorstackA.top() : " << OUTPUT << vectorstackA.top() << ENDL;
+	FILE << "vectorstackA.push(2);" << ENDL; vectorstackA.push(2);
+	FILE << "vectorstackA.top() : " << OUTPUT << vectorstackA.top() << ENDL;
+	FILE << "vectorstackA.pop();" << ENDL; vectorstackA.pop();
+	FILE << "vectorstackA.top() : " << OUTPUT << vectorstackA.top() << ENDL;
 
+	FILE << "stack<int, ft::vector<int> > vectorstackB(vectorstackA);" << ENDL; stack<int, ft::vector<int> > vectorstackB(vectorstackA);
+	FILE << "vectorstackB.top() : " << OUTPUT << vectorstackB.top() << ENDL;
+	FILE << "vectorstackB.empty() : " << std::boolalpha << OUTPUT << vectorstackB.empty() << ENDL;
+	FILE << "vectorstackB.size() : " << OUTPUT << vectorstackB.size() << ENDL;
+	FILE << "vectorstackA.push(42);" << ENDL; vectorstackA.push(42);
+	FILE << "vectorstackB.top() : " << OUTPUT << vectorstackB.top() << ENDL;
+	FILE << "vectorstackB = vectorstackA;" << ENDL; vectorstackB = vectorstackA;
+	FILE << "vectorstackB.top() : " << OUTPUT << vectorstackB.top() << ENDL;
+
+	FILE << ENDL;
+	FILE << "stack<int, ft::vector<int> > const vectorstackConst(vectorstackA);" << ENDL; stack<int, ft::vector<int> > const vectorstackConst(vectorstackA);
+	FILE << "vectorstackConst.top() : " << OUTPUT << vectorstackConst.top() << ENDL;
+	FILE << "vectorstackConst.empty() : " << std::boolalpha << OUTPUT << vectorstackConst.empty() << ENDL;
+	FILE << "vectorstackConst.size() : " << OUTPUT << vectorstackConst.size() << ENDL;
+
+	FILE << ENDL;
+	FILE << "stack<int, ft::vector<int> > vectorstackEmpty;" << ENDL; stack<int, ft::vector<int> > vectorstackEmpty;
+	FILE << "vectorstackConst == vectorstackA : " << std::boolalpha << OUTPUT << (vectorstackConst == vectorstackA) << ENDL;
+	FILE << "vectorstackConst == vectorstackEmpty : " << std::boolalpha << OUTPUT << (vectorstackConst == vectorstackEmpty) << ENDL;
+	FILE << "vectorstackConst != vectorstackA : " << std::boolalpha << OUTPUT << (vectorstackConst != vectorstackA) << ENDL;
+	FILE << "vectorstackConst != vectorstackEmpty : " << std::boolalpha << OUTPUT << (vectorstackConst != vectorstackEmpty) << ENDL;
+	FILE << "vectorstackConst < vectorstackA : " << std::boolalpha << OUTPUT << (vectorstackConst < vectorstackA) << ENDL;
+	FILE << "vectorstackEmpty < vectorstackConst: " << std::boolalpha << OUTPUT << (vectorstackEmpty < vectorstackConst) << ENDL;
+	FILE << "vectorstackConst <= vectorstackA : " << std::boolalpha << OUTPUT << (vectorstackConst <= vectorstackA) << ENDL;
+	FILE << "vectorstackEmpty <= vectorstackConst: " << std::boolalpha << OUTPUT << (vectorstackEmpty <= vectorstackConst) << ENDL;
+	FILE << "vectorstackConst > vectorstackA : " << std::boolalpha << OUTPUT << (vectorstackConst > vectorstackA) << ENDL;
+	FILE << "vectorstackEmpty > vectorstackConst: " << std::boolalpha << OUTPUT << (vectorstackEmpty > vectorstackConst) << ENDL;
+	FILE << "vectorstackConst >= vectorstackA : " << std::boolalpha << OUTPUT << (vectorstackConst >= vectorstackA) << ENDL;
+	FILE << "vectorstackEmpty >= vectorstackConst: " << std::boolalpha << OUTPUT << (vectorstackEmpty >= vectorstackConst) << ENDL;
 
 	FILE << TITLE << "=> ENDING stack tests" << ENDL << ENDL;
 }
@@ -67,5 +122,5 @@ test_stack(void)
 int
 main(void)
 {
-    test_stack<ft::stack>();
+    test_stack<std::stack>();
 }
